195ce6c70a0e774a102d17fbc1f790c9a48626f5
Will Utterback <uwill08@gmail.com>
2021-03-16 00:07:49 -0500

    Finished filling out file structure skeleton, basic functionality

---
+++ models/User.js
@@ -0,0 +1,42 @@
+const sequelize = require('../config/connection');
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+
+class User extends Model {
+  checkPassword(pw) {
+    return bcrypt.compareSync(pw, this.password);
+  }
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    hooks: {
+      async beforeCreate(user) {
+        user.password = await bcrypt.hash(user.password, 10);
+        return user;
+      },
+    },
+    tableName: 'users',
+    modelName: 'user',
+    sequelize,
+    timestamps: false,
+  }
+);
+
+module.exports = User;
