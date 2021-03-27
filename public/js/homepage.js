c63b1b9fabc32435e95b0dfe2ad43b1fdd9e9e2c
Will Utterback <uwill08@gmail.com>
2021-03-26 21:53:08 -0500

    Created homepage effect

---
+++ public/js/homepage.js
@@ -0,0 +1,12 @@
+function transitionEffect() {
+  const homepage = document.getElementById('homepage');
+  homepage.classList.add('transition');
+  setTimeout(() => {
+    homepage.classList.remove('transition');
+  }, 2000);
+  setTimeout(() => {
+    homepage.innerHTML = 'Click Sign-In to Begin';
+    homepage.classList.add('transition');
+  }, 6000);
+}
+transitionEffect();
